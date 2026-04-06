f289744d9dc56238fadbc1faa2ec7dacb6e9eaf3
Robert <robertczeres.pv@gmail.com>
2026-04-06 12:40:41 +0200

    Dodano interaktywne filtry: rozwijane sekcje dla okresu i kwoty

--- MonthlyBudget.Web/obj/Debug/net9.0/MonthlyBudget.Web.AssemblyInfo.cs
+++ MonthlyBudget.Web/obj/Debug/net9.0/MonthlyBudget.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonthlyBudget.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ee3acbd31cd3bdf17dad840edcb942f2736e09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e57463ed5d164c4f88a622ffa2b9d0d85d9e19a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonthlyBudget.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonthlyBudget.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
